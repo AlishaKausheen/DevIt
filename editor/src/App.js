f1709b9e4b67fdf272f02c9425970b59852e6885
AlishaKausheen <alishakausheen@gmail.com>
2023-12-14 23:44:05 -0800

    text not visible

--- editor/src/App.js
+++ editor/src/App.js
@@ -1,25 +1,7 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const App = () => {
+  return <div className="h-screen w-screen bg-red-400 font-extrabold text-cyan-500 flex items-center justify-center text-10xl">App hai</div>
+  
 }
-
 export default App;
