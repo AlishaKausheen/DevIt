3e2fde189b74c9e59c545b0c0cc329fcf99e112c
AlishaKausheen <alishakausheen@gmail.com>
2023-12-24 05:14:24 -0800

    router installed

--- editor/src/App.js
+++ editor/src/App.js
@@ -1,7 +1,9 @@
 import React from 'react'
 
 const App = () => {
-  return <div className="h-screen w-screen bg-red-400 font-extrabold text-cyan-500 flex items-center justify-center text-10xl">App hai</div>
-  
+  return(
+    <div className="h-screen w-screen bg-red-400 font-extrabold text-cyan-500 flex items-center justify-center text-10xl">
+      App hai</div>
+  );
 }
 export default App;
