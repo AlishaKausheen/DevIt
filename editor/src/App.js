d57c0548e95c3ff5059050b18cd3f63fa43fff6d
AlishaKausheen <alishakausheen@gmail.com>
2023-12-24 06:06:36 -0800

    react route navigation done

--- editor/src/App.js
+++ editor/src/App.js
@@ -1,12 +1,13 @@
 import React from 'react'
-import { Route, Routes } from "react-router-dom"
+import { Navigate, Route, Routes } from "react-router-dom"
 import {Home}  from './container';
 
 const App = () => {
   return(
     <div className="h-screen w-screen flex items-start justify-start overflow-hidden">
       <Routes>
-        <Route path='/home/*' element={<Home/>} />
+        <Route path='/home/*' element={<Home />} />
+        <Route path='*' element={<Navigate to={'/home'}/>}/>
       </Routes>
     </div>
   );
