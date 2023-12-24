f3efbba2858c4473069249af0258e074c0190360
AlishaKausheen <alishakausheen@gmail.com>
2023-12-24 08:49:44 -0800

    framer motion issue

--- editor/src/container/Home.jsx
+++ editor/src/container/Home.jsx
@@ -1,17 +1,20 @@
 import React, { useState } from 'react'
-import {HiChevronDoubleLeft} from "react-icons/hi2"
+import { HiChevronDoubleLeft } from "react-icons/hi2"
+import {motion} from "framer-motion"
 
 const Home = ()=> {
  const [isSideMenu , setIsSideMenu] = useState(false)
-    return (
+    return(
     <div>
-            <div className={`w-48 ${isSideMenu ? "w-48" : "flex-[.2] xl:flex-[1.8]"} 
-            min-h-screen max-h-screen relative bg-zinc-800`}>
+            <div className={`w-48 ${isSideMenu ? "w-4" : "flex-[.2] xl:flex-[.4]"} 
+            min-h-screen max-h-screen relative bg-zinc-800
+            px-3 py-6 flex flex-col items-center justify-start gap-4 transition-all
+            duration-200 ease-in-out`}>
                 {/* anchor*/}
-                <div className='w-8 h-8 bg-zinc-800 rounded-tr-lg 
+                <motion.div whileTap={{scale: 0.9}} onClick={() => setIsSideMenu(!isSideMenu)} className='w-8 h-8 bg-zinc-800 rounded-tr-lg 
                 rounded-br-lg absolute -right-8 flex items-center cursor-pointer'>
                  <HiChevronDoubleLeft className='text-white text-xl'/>
-                </div>
+                </motion.div>
                 {/*logo */}
                 {/*start coding */}
                 {/*home nav */}
