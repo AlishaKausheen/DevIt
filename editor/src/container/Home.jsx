5fddf8a28131ef7169c96103520337ea10c644a1
AlishaKausheen <alishakausheen@gmail.com>
2023-12-25 04:29:53 -0800

    home icon added

--- editor/src/container/Home.jsx
+++ editor/src/container/Home.jsx
@@ -1,14 +1,16 @@
 import React, { useState } from 'react'
 import { HiChevronDoubleLeft } from "react-icons/hi2"
+import { MdHome } from "react-icons/md"
 import {motion} from "framer-motion"
 import { Link } from 'react-router-dom'
 import {Logo} from "../assets"
 
 const Home = ()=> {
- const [isSideMenu , setIsSideMenu] = useState(false)
+  const [isSideMenu, setIsSideMenu] = useState(false);
+  const [user, setUser] = useState(null);
     return(
     <div>
-            <div className={`w-48 ${isSideMenu ? "w-4" : "flex-[.2] xl:flex-[.4]"} 
+            <div className={`w-54 ${isSideMenu ? "w-4" : "flex-[.2] xl:flex-[.4]"} 
             min-h-screen max-h-screen relative bg-zinc-800
             px-3 py-6 flex flex-col items-center justify-start gap-4 transition-all
             duration-200 ease-in-out`}>
@@ -20,10 +22,23 @@ const Home = ()=> {
           <div className='overflow-hidden w-full flex flex-col gap-4'>
             {/*logo */}
             <Link to={"/home"}>
-              <img src={Logo} alt="Logo" className='object-contain w-48 h-18' />
+              <img src={Logo} alt="Logo" className='object-contain w-48 h-auto' />
             </Link>
-                {/*start coding */}
+            {/*start coding */}
+            <Link to={'/newProject'}>
+              <div className='px-6 py-3 flex items-center justify-center
+              rounded-xl border border-gray-400 cursor-pointer
+              group hover:border-gray-200'>
+               <p className='text-gray-400 group-hover:text-gray-200 capitalize'>Start Coding</p>
+              </div>
+            </Link>
             {/*home nav */}
+            {user && (
+              <Link to={"/home/projects"} className='flex items-center justify-center gap-6'>
+                <MdHome className='text-gray-200 text-xl' />
+                <p className='text-lg text-gray-200'>Home</p>
+              </Link>
+            )}
             </div>
             </div>
             <div></div>
