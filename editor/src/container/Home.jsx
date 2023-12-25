bdd599a1804142be33df1c46ab66e62624ba97bf
AlishaKausheen <alishakausheen@gmail.com>
2023-12-25 05:48:52 -0800

    error something

--- editor/src/container/Home.jsx
+++ editor/src/container/Home.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from 'react'
 import { HiChevronDoubleLeft } from "react-icons/hi2"
 import { MdHome } from "react-icons/md"
+import { FaSearchengin } from "react-icons/fa6"
 import {motion} from "framer-motion"
 import { Link } from 'react-router-dom'
 import {Logo} from "../assets"
@@ -10,7 +11,7 @@ const Home = ()=> {
   const [user, setUser] = useState(null);
     return(
     <div>
-            <div className={`w-54 ${isSideMenu ? "w-4" : "flex-[.2] xl:flex-[.4]"} 
+            <div className={`w-48 ${isSideMenu ? "w-4" : "flex-[.2] xl:flex-[.4]"} 
             min-h-screen max-h-screen relative bg-zinc-800
             px-3 py-6 flex flex-col items-center justify-start gap-4 transition-all
             duration-200 ease-in-out`}>
@@ -41,7 +42,19 @@ const Home = ()=> {
             )}
             </div>
             </div>
-            <div></div>
+        <div className='flex-1 min-h-screen max-h-screen overflow-y-scroll
+            h-full flex flex-col items-start justify-start px-4 md:px-12'>
+          {/*top section */}
+          <div className='w-full flex items-center justify-between'>
+            {/* search */}
+            <div>
+              <FaSearchengin className='text-2xl text-gray-300' />
+              <input type='text' className='flex-1 px-4 py-1 text-xl bg-transparent
+              outline-none border-none text-gray-300 placeholder:text-gray-600' placeholder="Search here..." />
+            </div>
+            {/*profile section */}
+          </div>
+            </div>
     </div>
   )
 }
