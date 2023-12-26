9c24ccdb3657447067beba2352d00e1f3140bb27
AlishaKausheen <alishakausheen@gmail.com>
2023-12-26 00:24:35 -0800

    search box

--- editor/src/container/Home.jsx
+++ editor/src/container/Home.jsx
@@ -10,8 +10,8 @@ const Home = ()=> {
   const [isSideMenu, setIsSideMenu] = useState(false);
   const [user, setUser] = useState(null);
     return(
-    <div>
-            <div className={`w-48 ${isSideMenu ? "w-4" : "flex-[.2] xl:flex-[.4]"} 
+    <>
+            <div className={`w-16 ${isSideMenu ? "w-1" : "flex-[.1] xl:flex-[.2]"} 
             min-h-screen max-h-screen relative bg-zinc-800
             px-3 py-6 flex flex-col items-center justify-start gap-4 transition-all
             duration-200 ease-in-out`}>
@@ -23,7 +23,7 @@ const Home = ()=> {
           <div className='overflow-hidden w-full flex flex-col gap-4'>
             {/*logo */}
             <Link to={"/home"}>
-              <img src={Logo} alt="Logo" className='object-contain w-48 h-auto' />
+              <img src={Logo} alt="Logo" className='object-contain w-16 h-auto' />
             </Link>
             {/*start coding */}
             <Link to={'/newProject'}>
@@ -41,21 +41,35 @@ const Home = ()=> {
               </Link>
             )}
             </div>
-            </div>
-        <div className='flex-1 min-h-screen max-h-screen overflow-y-scroll
-            h-full flex flex-col items-start justify-start px-4 md:px-12'>
+        </div>
+        
+        <div  className='flex-1 min-h-screen max-h-screen overflow-y-scroll
+            h-full flex flex-col items-start justify-start px-4 md:px-12 py-4 md:py-12'>
           {/*top section */}
-          <div className='w-full flex items-center justify-between'>
+          <div className=' h-2 w-full flex items-center justify-between gap-3'>
             {/* search */}
-            <div>
+            <div className='bg-zinc-800 h-10 w-full px-4 py-3 rounded-md
+            flex items-center justify-center gap-3'>
               <FaSearchengin className='text-2xl text-gray-300' />
               <input type='text' className='flex-1 px-4 py-1 text-xl bg-transparent
               outline-none border-none text-gray-300 placeholder:text-gray-600' placeholder="Search here..." />
             </div>
             {/*profile section */}
+            {!user && (
+              <div className='flex items-center justify-center gap-3'>
+                <Link to={"/home/auth"} className='bg-emerald-500 px-6 py-2
+                rounded-md text-white text-lg cursor-pointer hover:bg-emerald-700'>
+                  SignUp
+                </Link>
+                </div>
+            )}
+
+            {user && (
+              <div></div>
+            )}
           </div>
-            </div>
-    </div>
+          </div>  
+    </>
   )
 }
 
