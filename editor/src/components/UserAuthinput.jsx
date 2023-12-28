5981fca6ab239b14ff75119d402a317237afa5b2
AlishaKausheen <alishakausheen@gmail.com>
2023-12-27 23:23:29 -0800

    email input

--- editor/src/components/UserAuthinput.jsx
+++ editor/src/components/UserAuthinput.jsx
@@ -1,9 +1,20 @@
-import React from 'react'
+import React, { useState } from 'react'
+import {FaEnvelope} from "react-icons/fa6"
 
 const UserAuthinput = () => {
+    const [value, setValue] = useState("");
   return (
     <div className='flex flex-col items-start justify-start gap-1'>
-      
+          <label className='text-sm text-gray-300'>Email</label>
+          <div className={`flex items-center justify-center gap-3 w-full md:w-96
+          rounded-md px-4 py-1 bg-gray-200`}>
+              <FaEnvelope className='text-text555 text-2xl' />
+              <input type='text'
+                  placeholder='Email here'
+                  className='flex-1 w-full h-full py-6 outline-none border-none bg-transparent text-gray-600 text-lg'
+                  value={value}
+              onChange={(e)=>setValue(e.target.value)}/>
+          </div>
     </div>
   )
 }
