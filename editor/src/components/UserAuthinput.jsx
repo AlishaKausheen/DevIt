e4c3266219afa91d1ae6756da3c0dfd67656348e
AlishaKausheen <alishakausheen@gmail.com>
2023-12-27 22:33:56 -0800

    making reusuable component

---
+++ editor/src/components/UserAuthinput.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const UserAuthinput = () => {
+  return (
+    <div className='flex flex-col items-start justify-start gap-1'>
+      
+    </div>
+  )
+}
+
+export default UserAuthinput
--- editor/src/container/SignUp.jsx
+++ editor/src/container/SignUp.jsx
@@ -1,14 +1,32 @@
 import React from 'react'
 import { Logo } from '../assets';
+import { UserAuthinput } from '../components';
 
 const SignUp = () => {
   return (
       <div className='w-full py-6'>
-          <img src={Logo} className='object-contain w-12 opacity-50 h-auto' alt="" />
+          <img src={Logo} className='object-contain w-12 opacity-50 h-auto' alt="Logo" />
           <div className='w-full flex flex-col items-center justify-center py-8'>
-              <p>Join with us...</p>
+              <p className='py-12 text-2xl text-gray-200'>Join with us...</p>
               <div className='px-8 w-full md:w-auto py-4 rounded-xl bg-slate-700
               shadow-md flex flex-col items-center justify-center gap-8'>
+          {/*email */}
+           <UserAuthinput/>
+          {/*password */}
+
+          {/*alert section */}
+
+          {/*login button */}
+
+          {/*account text  section */}
+
+          {/*or section */}
+
+          {/* sign in with google */}
+
+          {/*or section */}
+
+          {/*sign in with github */}
                   
               </div>
 
