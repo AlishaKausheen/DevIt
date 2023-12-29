a49c9c5e2ca27a410fd38e35a91473008fa3e194
AlishaKausheen <alishakausheen@gmail.com>
2023-12-28 23:02:47 -0800

    password iconfield fixed

--- editor/src/components/UserAuthinput.jsx
+++ editor/src/components/UserAuthinput.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { FaEnvelope, FaEye } from "react-icons/fa6";
+import {FaEye, FaEyeSlash } from "react-icons/fa6";
 import { motion } from "framer-motion";
 
 const UserAuthinput = ({ label, placeHolder, isPass, setStateFunction, Icon }) => {
@@ -11,15 +11,18 @@ const UserAuthinput = ({ label, placeHolder, isPass, setStateFunction, Icon }) =
           <div className={`flex items-center justify-center gap-3 w-full md:w-96
           rounded-md px-4 py-1 bg-gray-200`}>
               <Icon className='text-text555 text-2xl' />
-              <input type= {isPass ? "password" : "text"}
+              <input type= {isPass && showPass ? "password" : "text"}
                   placeholder={placeHolder}
                   className='flex-1 w-full h-full py-6 outline-none border-none bg-transparent text-gray-600 text-lg'
                   value={value}
                   onChange={(e) => setValue(e.target.value)} />
         {isPass && (
-          <motion.div whileTap={{scale: .9}} className='cursor-pointer'>
-                  <FaEye className='text-gray-600 text-2xl'/>
-              </motion.div>
+          <motion.div onClick={()=> setShowPass(!showPass)} whileTap={{scale: .9}} className='cursor-pointer'>
+            {showPass ?
+                 ( <FaEye className='text-gray-600 text-2xl'/>)
+              : (<FaEyeSlash className='text-gray-600 text-2xl'/>)
+            }
+          </motion.div>
               )}
           </div>
     </div>
