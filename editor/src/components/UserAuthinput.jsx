f54434cbff23dc6d599022fd80eaa8969925c79c
AlishaKausheen <alishakausheen@gmail.com>
2023-12-28 22:44:43 -0800

    fixed motion

--- editor/src/components/UserAuthinput.jsx
+++ editor/src/components/UserAuthinput.jsx
@@ -1,8 +1,10 @@
-import React, { useState } from 'react'
-import {FaEnvelope , FaEye} from "react-icons/fa6"
+import React, { useState } from 'react';
+import { FaEnvelope, FaEye } from "react-icons/fa6";
+import { motion } from "framer-motion";
 
 const UserAuthinput = ({ label, placeHolder, isPass, setStateFunction, Icon }) => {
-    const [value, setValue] = useState("");
+  const [value, setValue] = useState("");
+  const [showPass, setShowPass] = useState(false);
   return (
     <div className='flex flex-col items-start justify-start gap-1'>
           <label className='text-sm text-gray-300'>{label}</label>
@@ -14,9 +16,11 @@ const UserAuthinput = ({ label, placeHolder, isPass, setStateFunction, Icon }) =
                   className='flex-1 w-full h-full py-6 outline-none border-none bg-transparent text-gray-600 text-lg'
                   value={value}
                   onChange={(e) => setValue(e.target.value)} />
-              <div className='cursor-pointer'>
+        {isPass && (
+          <motion.div whileTap={{scale: .9}} className='cursor-pointer'>
                   <FaEye className='text-gray-600 text-2xl'/>
-              </div>
+              </motion.div>
+              )}
           </div>
     </div>
   )
