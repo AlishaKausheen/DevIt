4c5139956ddd765e75ad022d80db344f07294bd2
AlishaKausheen <alishakausheen@gmail.com>
2023-12-31 06:53:49 -0800

    firebase config

---
+++ editor/src/config/firebase.config.js
@@ -0,0 +1,10 @@
+
+
+const firebaseConfig = {
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTHDOMAIN,
+  projectId: process.env.REACT_APP_PROJECTID,
+  storageBucket: process.env.REACT_APP_STORAGEBUCKET,
+  messagingSenderId:  process.env.REACT_APP_MESSAGESENDERID,
+  appId: process.env.REACT_APP_APPID,
+};
