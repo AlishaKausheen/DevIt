280381de22d44071aa25bfbbb0fb9aaac14add85
AlishaKausheen <alishakausheen@gmail.com>
2024-01-03 23:18:00 -0800

    usercred

--- editor/src/App.js
+++ editor/src/App.js
@@ -1,8 +1,19 @@
-import React from 'react'
-import { Navigate, Route, Routes } from "react-router-dom"
+import React, { useEffect } from 'react'
+import { Navigate, Route, Routes, useNavigate } from "react-router-dom"
 import {Home}  from './container';
 
 const App = () => {
+  const navigate = useNavigate();
+  useEffect(() => {
+    const unsubscribe = auth.onAuthStateChanged(userCred => {
+      if (userCred) {
+        console.log(userCred?.providerData[0].email);
+      }
+      else {
+        navigate("/home/auth", {replace:true})
+      }
+    })
+  }, [])
   return(
     <div className="h-screen w-screen flex items-start justify-start overflow-hidden">
       <Routes>
