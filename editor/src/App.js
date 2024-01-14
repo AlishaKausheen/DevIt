5cc3c25b7ffbd7f6365125f9d15ed03d4756fa5b
AlishaKausheen <alishakausheen@gmail.com>
2024-01-14 06:59:34 -0800

    dispatch action

--- editor/src/App.js
+++ editor/src/App.js
@@ -2,6 +2,7 @@ import React, { useEffect } from 'react'
 import { Navigate, Route, Routes, useNavigate } from "react-router-dom"
 import { Home } from './container';
 import { auth } from './config/firebase.config';
+import { setDoc } from 'firebase/firestore';
 
 
 const App = () => {
@@ -10,11 +11,19 @@ const App = () => {
     const unsubscribe = auth.onAuthStateChanged(userCred => {
       if (userCred) {
         console.log(userCred?.providerData[0]);
+        setDoc(doc(db, "users", userCred?.uid), userCred?.providerData[0]).
+          then(() => {
+            //dispath the action to store
+
+          })
       }
       else {
         navigate("/home/auth", {replace:true})
       }
     })
+
+    //clean up the listener event
+    return () => unsubscribe();
   }, [])
   return(
     <div className="h-screen w-screen flex items-start justify-start overflow-hidden">
