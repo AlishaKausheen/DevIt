4a3853eafcac2c2f0a1ea12e95dff881c7b257f7
AlishaKausheen <alishakausheen@gmail.com>
2024-01-27 00:14:37 -0800

    will work on icon in the evening

--- editor/src/components/UserProfileDetails.jsx
+++ editor/src/components/UserProfileDetails.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
-import { UseSelector } from 'react-redux';
+import {  useSelector } from 'react-redux';
 import { motion } from 'framer-motion';
+import { FaChevronDown } from 'react-icons/fa6';
 
 
 const UserProfileDetails = () => {
@@ -9,14 +10,17 @@ const UserProfileDetails = () => {
     <div className='flex items-center justify-center gap-4 relative'>
       <div className='w-14 h-14 flex items-center justify-center rounded-xl overflow-hidden cursor-pointer bg-emerald-500'>
         {
-          user?.photoURL ? <>
-            <motion.img whileHover={{ scale: 1.2 }} src={user?.photoURL}
+          user?.photoURL ? (<>
+            <motion.img whileHover={{ scale: 1.3 }} src={user?.photoURL}
               alt={user?.displayName} referrerPolicy='no-referrer' className='w-full h-full object-cover'/>
-          </> : <p className='text-xl text-white font-semibold capitalize'>
+          </> ): (<p className='text-xl text-white font-semibold capitalize'>
               {user?.email[0]}
           </p>
-        }
+        )}
       </div>
+      <motion.div whileTap={{scale: 0.9}}>
+        <FaChevronDown className=' text-gray-400'/>
+      </motion.div>
     </div>
   )
 }
