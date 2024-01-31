cd0c5ff380eb259978dc36e42ed8b13dbbe0ab53
AlishaKausheen <alishakausheen@gmail.com>
2024-01-31 08:41:58 -0800

    will work on code mirror

--- editor/src/container/NewProject.jsx
+++ editor/src/container/NewProject.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import SplitPane from "react-split-pane";
-import { FaHtml5 } from "react-icons/fa6";
+import { FaChevronDown, FaCss3Alt, FaHtml5, FaJs } from "react-icons/fa6";
+import { FcSettings } from 'react-icons/fc';
 
 const NewProject = () => {
     return <>
@@ -18,10 +19,15 @@ const NewProject = () => {
                         {/* html code */}
                         <div className='w-full h-full flex flex-col items-start justify-start '>
                             <div className='w-full flex items-center justify-between '>
-                                <div className='bg-zinc-800 px-4 py-2 border-t-4 flex items-center justify-center gap-3'>
+                                <div className='bg-zinc-800 px-4 py-2 border-t-4 flex items-center justify-center gap-3 border-t-gray-500'>
                                     <FaHtml5 className='text-xl text-red-500' />
-                                    <p className='text-gray-300 font-semibold border-t-gray-500'>HTML</p>
-                            </div>
+                                    <p className='text-gray-300 font-semibold '>HTML</p>
+                                </div>
+                                {/*icons */}
+                                <div className='cursor-pointer flex items-center gap-5 px-4 '>
+                                    <FcSettings className='text-xl ' />
+                                    <FaChevronDown className='text-xl text-gray-300'/>
+                                </div>
                             </div>
                             <div>Code Mirror</div>
 
@@ -29,8 +35,38 @@ const NewProject = () => {
 
                         <SplitPane split='vertical' minSize={500}>
                             {/*css code */}
+                            <div className='w-full h-full flex flex-col items-start justify-start '>
+                            <div className='w-full flex items-center justify-between '>
+                                <div className='bg-zinc-800 px-4 py-2 border-t-4 flex items-center justify-center gap-3 border-t-gray-500'>
+                                    <FaCss3Alt className='text-xl text-sky-500' />
+                                    <p className='text-gray-300 font-semibold '>CSS</p>
+                                </div>
+                                {/*icons */}
+                                <div className='cursor-pointer flex items-center gap-5 px-4 '>
+                                    <FcSettings className='text-xl ' />
+                                    <FaChevronDown className='text-xl text-gray-300'/>
+                                </div>
+                            </div>
+                            <div>Code Mirror</div>
+
+                        </div>
                             
                             {/* js code */}
+                            <div className='w-full h-full flex flex-col items-start justify-start '>
+                            <div className='w-full flex items-center justify-between '>
+                                <div className='bg-zinc-800 px-4 py-2 border-t-4 flex items-center justify-center gap-3 border-t-gray-500'>
+                                    <FaJs className='text-xl text-yellow-500' />
+                                    <p className='text-gray-300 font-semibold '>JS</p>
+                                </div>
+                                {/*icons */}
+                                <div className='cursor-pointer flex items-center gap-5 px-4 '>
+                                    <FcSettings className='text-xl ' />
+                                    <FaChevronDown className='text-xl text-gray-300'/>
+                                </div>
+                            </div>
+                            <div>Code Mirror</div>
+
+                        </div>
                         </SplitPane>
                      
                     </SplitPane>
