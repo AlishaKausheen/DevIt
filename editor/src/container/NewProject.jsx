d77e733722f2469a7c8ece532554d24890537166
AlishaKausheen <alishakausheen@gmail.com>
2024-01-31 08:20:32 -0800

    styling react split pane

--- editor/src/container/NewProject.jsx
+++ editor/src/container/NewProject.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import SplitPane from "react-split-pane";
+import { FaHtml5 } from "react-icons/fa6";
 
 const NewProject = () => {
     return <>
@@ -15,10 +16,20 @@ const NewProject = () => {
                     {/*top coding section */}
                     <SplitPane split='vertical' minSize={500}>
                         {/* html code */}
+                        <div className='w-full h-full flex flex-col items-start justify-start '>
+                            <div className='w-full flex items-center justify-between '>
+                                <div className='bg-zinc-800 px-4 py-2 border-t-4 flex items-center justify-center gap-3'>
+                                    <FaHtml5 className='text-xl text-red-500' />
+                                    <p className='text-gray-300 font-semibold border-t-gray-500'>HTML</p>
+                            </div>
+                            </div>
+                            <div>Code Mirror</div>
 
-                        <SplitPane>
+                        </div>
+
+                        <SplitPane split='vertical' minSize={500}>
                             {/*css code */}
-
+                            
                             {/* js code */}
                         </SplitPane>
                      
