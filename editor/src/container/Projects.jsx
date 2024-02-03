9c4020deb53faebfc3727291aeea5a510b8213d8
AlishaKausheen <alishakausheen@gmail.com>
2024-02-03 00:08:48 -0800

    projectcard container

--- editor/src/container/Projects.jsx
+++ editor/src/container/Projects.jsx
@@ -1,9 +1,22 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
 
- const Projects=()=> {
+const Projects = () => {
+  const projects = useSelector((state) => state.projects?.projects);
   return (
-    <div className='text-gray-300'>
-      Projects
+    <div className='w-full py-6 flex items-center justify-center gap-6 flex-wrap'>
+      {projects && projects.map((project, index) => (
+        <ProjectCard key={project.id} project={project} index={index} />
+      ))}
+      
+    </div>
+  )
+}
+
+const ProjectCard = () => {
+  return (
+    <div>
+      Project
     </div>
   )
 }
