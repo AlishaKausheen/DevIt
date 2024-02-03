18cbb8d3529db9dbe7e22d69d9380a4b77d5e35a
AlishaKausheen <alishakausheen@gmail.com>
2024-02-03 07:49:34 -0800

    output screen optimization

--- editor/src/container/Projects.jsx
+++ editor/src/container/Projects.jsx
@@ -17,8 +17,8 @@ const Projects = () => {
 const ProjectCard = ({project, index}) => {
   return (
     <motion.div key={index} className='w-full cursor-pointer md:w[450px]
-    h-[375px] bg-zinc-700 rounded-md p-4 flex flex-col items-center justify-center
-    gap-4'>
+    h-[375px] bg-zinc-700 rounded-md p-3 flex flex-col items-center justify-center
+    gap-2'>
       <div className='bg-zinc-700 w-full h-full rounded-md overflow-hidden'
         style={{ overflow: "hidden", height: "100%" }}>
                         <iframe
@@ -26,7 +26,28 @@ const ProjectCard = ({project, index}) => {
                             srcDoc={project.output}
                             style={{border: 'none', width:'100%', height:'100%'}}
                         />
-                    </div>
+      </div>
+      <div className='flex items-center justify-start gap-2 w-full'>
+        {/*image */}
+        <div className='w-14 h-14 flex items-center justify-center 
+rounded-xl overflow-hidden cursor-pointer bg-emerald-500'>
+          {project?.user?.photoURL ? (
+          <motion.img
+            whileHover={{ scale: 1.2 }}
+            src={project?.user?.photoURL}
+            alt=''
+            referrerPolicy='no-referrer'
+            className='w-full h-full object-cover'
+
+          />
+        ) : (
+            <p className='text-xl text-white font-semibold capitalize'>{project?.user?.email[0]}</p>
+         )}
+
+</div>
+
+                {/*name */}
+      </div>
     </motion.div>
   )
 }
